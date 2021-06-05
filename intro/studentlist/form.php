64966998241ad538f81bf0f49cca6229e0e83ddb
Uğur Arıcı <m.ugurarici@gmail.com>
2021-06-05 10:18:50 +0300

    Update studentlist example with array sort functions

--- intro/studentlist/form.php
+++ intro/studentlist/form.php
@@ -1,5 +1,5 @@
 <form action="result.php" method="post">
-    <?php for ($i = 1; $i <= 3; $i++) : ?>
+    <?php for ($i = 1; $i <= 6; $i++) : ?>
         <?= $i ?>.
         <input type="text" name="students[]" placeholder="Öğrenci Adı">
         <input type="number" min="0" max="100" name="notes[]" placeholder="Not">
--- intro/studentlist/result.php
+++ intro/studentlist/result.php
@@ -1,3 +1,20 @@
+<?php
+
+//  bu sayfaya bir form üzerinden students ve notes dizileri geliyor
+//  ikisi bağımsız diziler olmakla birlikte, aynı indise sahip elemanların ilişkisi var
+//  yani students dizisinin 0 indisli elemanı öğrencinin adını, notes dizisinin 0 indisli elemanı o öğrencinin notunu içeriyor
+
+//  bunları ilişkili şekilde göstermek istiyoruz
+//  nota göre büyükten küçüğe listelemek istiyoruz
+//  PHP'de listeleme fonksiyonları için resmi belgesi referans alınabilir: https://www.php.net/manual/tr/array.sorting.php
+
+var_dump($_POST['notes']);
+
+arsort($_POST['notes']);
+
+var_dump($_POST['notes']);
+
+?>
 <table border="1" cellpadding="5">
     <thead>
         <tr>
@@ -6,10 +23,10 @@
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($_POST['students'] as $key => $value) : ?>
+        <?php foreach ($_POST['notes'] as $key => $value) : ?>
             <tr>
+                <td><?= $_POST['students'][$key] ?></td>
                 <td><?= $value ?></td>
-                <td><?= $_POST['notes'][$key] ?></td>
             </tr>
         <?php endforeach; ?>
     </tbody>
