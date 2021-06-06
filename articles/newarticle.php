40c2d44896befb6739bd37745eb296288da5a802
Uğur Arıcı <m.ugurarici@gmail.com>
2021-06-06 10:58:37 +0300

    Update new article form

--- articles/newarticle.php
+++ articles/newarticle.php
@@ -1,12 +1,15 @@
 <?php include "header.php"; ?>
 <div class="container">
-    <form action="createarticle.php" class="form" method="post">
-        Title: <input type="text" name="title">
-        <br>
-        Content:<br>
-        <textarea name="content"></textarea>
-        <br>
-        <button type="submit">Save New Article</button>
+    <form action="createarticle.php" method="post">
+        <div class="mb-3">
+            <label for="inpTitle" class="form-label">Title</label>
+            <input type="text" class="form-control" id="inpTitle" name="title">
+        </div>
+        <div class="mb-3">
+            <label for="txtContent" class="form-label">Content</label>
+            <textarea class="form-control" id="txtContent" rows="3" name="content"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Save New Article</button>
     </form>
 </div>
 <?php include "footer.php"; ?>
