006d3e33720cd0609ed220a97788b5355df6e186
Uğur Arıcı <m.ugurarici@gmail.com>
2021-06-19 11:25:18 +0300

    Refactor create and update forms and actions to one file

--- articles/editarticle.php
+++ articles/editarticle.php
@@ -3,6 +3,35 @@ require "init.php";
 
 redirectIfNotLoggedIn();
 
+//  eskiden updatearticle.php içinde bulunan kodları buraya alıyoruz
+//  böylece düzenleme işlemi aşamaları olan form gösterme ve düzenlemeleri kaydetme işlemlerini tek dosyada topluyoruz
+//  aynı işlemi newarticle.php için de yapmıştık
+if (isset($_POST['id']) and isset($_POST['title']) and isset($_POST['content'])) {
+    if (!isset($_POST['id']) || !isset($articles[$_POST['id']])) {
+        header("Location: index.php");
+        die();
+    }
+
+    $articleId = $_POST["id"];
+
+    //  articles dizisindeki bir elemanı güncelliyoruz
+    $articles[$articleId] = [
+        "title" => $_POST['title'],
+        "content" => $_POST['content']
+    ];
+
+    //  articles dizisinin yeni halini JSON'a çeviriyoruz
+    $articlesJson = json_encode($articles);
+
+    //  JSON'ın yeni halini dosyaya yazıyoruz
+    file_put_contents('articles.json', $articlesJson);
+
+    //  burada işimiz bitti, güncellenen article detayına gidelim
+    header("Location: detail.php?id=" . $articleId);
+    die();
+}
+
+
 if (!isset($_GET['id']) || !isset($articles[$_GET['id']])) {
     header("Location: index.php");
     die();
@@ -17,7 +46,7 @@ $documentTitle = "Edit: " . $article['title'];
 include "header.php";
 ?>
 <div class="container">
-    <form action="updatearticle.php" method="post">
+    <form method="post">
         <input type="hidden" name="id" value="<?= $articleId ?>">
         <div class="mb-3">
             <label for="inpTitle" class="form-label">Title</label>
--- articles/newarticle.php
+++ articles/newarticle.php
@@ -3,10 +3,45 @@ require "init.php";
 
 redirectIfNotLoggedIn();
 
+//  eskiden createarticle.php şeklinde başka bir dosyada bulunan ekleme işlemini
+//  aynı dosyanın üstüne aldık
+//  öncelikle POST üstünden gelen başlık ve içerik var mı diye bakıyoruz,
+//  varsa ekleme işlemini yapıp detayına yönlendiriyoruz
+//  eğer yoksa zaten bu if bloğu içine girmez ve aşağıdaki form gözükür
+//  böylece yeni bir kayıt ekleme işlemine dair ihtiyaçlarımızı tek dosyada toplamış olduk
+if (isset($_POST['title']) and isset($_POST['content'])) {
+
+    //  articles dizisine yeni bir eleman ekliyoruz
+    $articles[] = [
+        "title" => $_POST['title'],
+        "content" => $_POST['content']
+    ];
+
+    //  articles dizisinin yeni halini JSON'a çeviriyoruz
+    $articlesJson = json_encode($articles);
+
+    //  JSON'ın yeni halini dosyaya yazıyoruz
+    file_put_contents('articles.json', $articlesJson);
+
+    //  son article elemanının indisini alalım
+    // $lastArticleId = count($articles) - 1;
+
+    if (function_exists('array_key_last')) {
+        $lastArticleId = array_key_last($articles);
+    } else {
+        //   array_last_key PHP 7.3 ve üstünde bulunduğu için, daha alt sürümler için alttaki 2 satır kullanılarak son dizi elemanının indisine erişilebilir
+        end($articles);
+        $lastArticleId = key($articles);
+    }
+
+    //  burada işimiz bitti, son aritcle detayına gidelim
+    header("Location: detail.php?id=" . $lastArticleId);
+}
+
 ?>
 <?php include "header.php"; ?>
 <div class="container">
-    <form action="createarticle.php" method="post">
+    <form method="post">
         <div class="mb-3">
             <label for="inpTitle" class="form-label">Title</label>
             <input type="text" class="form-control" id="inpTitle" name="title">
